82784638164056a70877610fc99ef6dd8c9cf1c8
Jack <jack@digilabs.io>
2018-09-07 16:07:51 -0500

    new integral gain

--- src/openrover_basic.cpp
+++ src/openrover_basic.cpp
@@ -153,9 +153,9 @@ OpenRover::OpenRover( ros::NodeHandle &_nh, ros::NodeHandle &_nh_priv ) :
     low_speed_mode_on_(true),
     velocity_control_on_(true),
     K_P_L_(40.0),
-    K_I_L_(15.0),
+    K_I_L_(1.0),
     K_P_R_(40.0),
-    K_I_R_(15.0),
+    K_I_R_(1.0),
     left_err_(0),
     right_err_(0)
 {
@@ -741,7 +741,7 @@ void OpenRover::velocityController()
     right_err_ = right_vel_commanded_ - right_vel_measured_;
 
     float left_motor_speed = K_P_L_ * left_err_ + K_I_L_ * (left_err_ - last_left_err) + 125;
-    float right_motor_speed = K_P_R_ * right_err_ + K_I_R_ * (right_err_ - last_right_err) + 125;
+    float right_motor_speed = K_P_L_ * right_err_ + K_I_L_ * (right_err_ - last_right_err) + 125;
 
     /*float left_motor_speed = (K_P_L_ * left_err_ ) + 125;
     float right_motor_speed = (K_P_R_ * right_err_) + 125;*/
