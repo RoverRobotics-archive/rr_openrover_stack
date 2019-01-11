605b76517aafd8434a95708f19b0aecfdae7ab0a
jack <jack@digilabs.io>
2019-01-10 18:45:20 -0600

    2wd teleop launch and reduced linear speed

--- src/openrover_basic.cpp
+++ src/openrover_basic.cpp
@@ -69,10 +69,10 @@ const float ODOM_AXLE_TRACK_2WD  = 14.375; //distance between wheels
 const float ODOM_ANGULAR_COEF_2WD = 1.0/(ODOM_AXLE_TRACK_2WD*2.54/100); //rad per meter
 const float ODOM_TRACTION_FACTOR_2WD = 0.9877;
 //low speed mode cmd_vel to motor command 2wd constants
-const int MOTOR_SPEED_LINEAR_COEF_2WD_LS = 293;
+const int MOTOR_SPEED_LINEAR_COEF_2WD_LS = 100;
 const int MOTOR_SPEED_ANGULAR_COEF_2WD_LS = 56;
 //high speed cmd_vel to motor command 2wd constants
-const int MOTOR_SPEED_LINEAR_COEF_2WD_HS = 293;
+const int MOTOR_SPEED_LINEAR_COEF_2WD_HS = 100;
 const int MOTOR_SPEED_ANGULAR_COEF_2WD_HS = 56;
 
 // Velocity Controller Constants
